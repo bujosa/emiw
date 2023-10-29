971c7b5a030e33e0471516f77b371e8ff4be8bfa
bujosa <davidbujosa@gmail.com>
2023-10-29 03:34:07 -0400

    test(math): implement spec for division method

---
+++ math/division_test.go
@@ -0,0 +1,13 @@
+package math
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDivision(t *testing.T) {
+	assert.Equal(t, 2.0, Division(4, 2), "4 / 2 should be 2.0")
+
+	assert.NotEqual(t, 3.0, Division(5, 2), "5 / 2 should not be 3.0")
+}
